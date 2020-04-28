22d6fcd43afcc5a98a8e26191d84b1f1a8e6377e
Gregor Werum <gregor.werum@web.de>
2020-04-28 12:37:25 +0200

    Active Directory update

--- 04_Active_Directory/ActiveDirectory.py
+++ 04_Active_Directory/ActiveDirectory.py
@@ -1,45 +1,157 @@
-class Group(object):
+import unittest
+import pandas as pd
+
+class Group():
     def __init__(self, _name):
         self.name = _name
         self.groups = []
-        self.users = []
+        self.members = []
+
+    def __repr__(self, level=0):
+        ret = "\t"*level+repr(self.name)+"\n"
+        for group in self.groups:
+            ret += group.__repr__(level+1)
+        return ret
 
     def add_group(self, group):
         self.groups.append(group)
 
-    def add_user(self, user):
-        self.users.append(user)
+    def add_member(self, member):
+        self.members.append(member)
 
     def get_groups(self):
         return self.groups
 
-    def get_users(self):
-        return self.users
+    def get_members(self):
+        return self.members
 
     def get_name(self):
         return self.name
 
-def is_user_in_group(user, group):
+def is_member_in_group(member, group):
     """
-    Return True if user is in the group, False otherwise.
+    Return True if member is in the group, False otherwise.
 
     Args:
-      user(str): user name/id
-      group(class:Group): group to check user membership against
+      member(str): member name/id
+      group(class:Group): group to check member membership against
     """
-    if user in group.get_users():
+    if member in group.get_members():
         return True
-    
+    for sub_group in group.get_groups():
+        if is_member_in_group(member, sub_group):
+            return True
+    return False
 
-    return None
+
+class TestActiveDirectory(unittest.TestCase):
+    """docstring for TestActiveDirectory"""
+    def test_family_directory(self):
+        print("\n ############# Test case 1: Test family memberships ########### ")
+        print("Input dictionary:")
+        dictionary = {}
+        dictionary['Parents'] = \
+            {'Members': ['Grandpa','Grandma'], 'Groups': ['Children']}
+        dictionary['Children'] = \
+            {'Members': ['Father', 'Mother', 'Uncle_1', 'Aunt_1', 'Uncle_2','Aunt_2'],\
+             'Groups': ['Childrens_Children']}
+        dictionary['Childrens_Children'] = \
+            {'Members': ['Son_1', 'Son_2', 'Daugther_1', 'Daugther_2', 'Cousin_male','Cousin_female'],\
+             'Groups': [] }
+        print(dictionary)
+
+        directory = self.create_directory_from(dictionary, 'Parents')
+        self.run_test(dictionary, directory, 'Family')
+        del directory
+        pass
+
+    def test_number_directory(self):
+        print("\n ############# Test case 2: Test number memberships ########### ")
+        print("Input dictionary:")
+        dictionary = {}
+        dictionary['Is smaller 1000'] = \
+            {'Members': [349, 874, 652, 492, 568], 'Groups': ['Is smaller 100', 'Is smaller 300']}
+        dictionary['Is smaller 300'] = \
+            {'Members': [100, 178, 287, 198, 234],\
+             'Groups': ['Is smaller 10', 'Is smaller 100']}
+        dictionary['Is smaller 100'] = \
+            {'Members': [10, 17, 45, 67, 23, 50, 93, 71, 85],\
+             'Groups': ['Is smaller 10']}
+        dictionary['Is smaller 10'] = \
+            {'Members': [2, 5, 6, 7, 8, 9],\
+             'Groups': [] }
+        print(dictionary)
+
+        directory = self.create_directory_from(dictionary, 'Is smaller 1000')
+        self.run_test(dictionary, directory, 'Numbers')
+        del directory
+        pass
+
+    def create_directory_from(self, dictionary, parent_name):
+        directory = Group(parent_name)
+        for member in dictionary[parent_name]['Members']:
+            directory.add_member(member)
+        for group_name in dictionary[parent_name]['Groups']:
+            directory.add_group(self.create_directory_from(dictionary, group_name))
+        return directory
+        
+    def run_test(self, dictionary, directory, directory_name):
+        members = self.get_all_members_from(directory)
+        groups = self.get_all_groups_from(directory)
+        print("\nThe '"+directory_name+"' directory has the following members: ")
+        print(members)
+        print("\nThe '"+directory_name+"' directory has the following group hierarchy: ")
+        print(directory)
+        # Run test an check if memberships are correctly returned
+        memberships = {'Members': members}
+        for group in groups.keys():
+            group_membership = []
+            for member in members:
+                is_member = is_member_in_group(member, groups[group])
+                self.assertEqual(is_member, self.expected_membership(member, group, dictionary))
+                group_membership.append(is_member)
+            memberships[group] = group_membership
+        df = pd.DataFrame(memberships)
+        df.set_index('Members', inplace=True, drop=True)
+        print("\nThe members of the '"+directory_name+"' directory have the following memberships:")
+        print(df)
+
+    def expected_membership(self, member, group, dictionary):
+        member_in_group = self.is_in_group(member, group, dictionary)
+        is_member_in_subgroup = self.is_in_subgroup(member, group, dictionary)
+        return (member_in_group or is_member_in_subgroup)
+
+    def is_in_group(self, member, group, dictionary):
+        members = dictionary[group]['Members']
+        if member in members:
+            return True
+        return False
+
+    def is_in_subgroup(self, member, group, dictionary):
+        if self.is_in_group(member, group, dictionary):
+            return True
+        is_member = 0
+        for sub_group in dictionary[group]['Groups']:
+            if self.is_in_subgroup(member, sub_group, dictionary):
+                is_member += 1
+        if is_member > 0:
+            return True
+        return False
+
+    def get_all_members_from(self, group):
+        member_list = []
+        for member in group.get_members():
+            member_list.append(member)
+        for group in group.get_groups():
+            member_list += self.get_all_members_from(group)
+        return member_list
+
+    def get_all_groups_from(self, group):
+        group_dict = {group.name: group}
+        for sub_group in group.get_groups():
+            group_dict.update(self.get_all_groups_from(sub_group))
+        return group_dict
 
 
-parent = Group("parent")
-child = Group("child")
-sub_child = Group("subchild")
 
-sub_child_user = "sub_child_user"
-sub_child.add_user(sub_child_user)
 
-child.add_group(sub_child)
-parent.add_group(child)
---
+++ 04_Active_Directory/explanation_4.md
@@ -0,0 +1,46 @@
+## Explanation for union_intersection.py
+
+### Code explanation & Data Structures
+
+The goal of this exercise is to implement the method **is_member_in_group(*member, group*)**, which searches an *Active Directory*-type tree structure and checks for membership. Each node of the tree represents a group, each group has a name, a list of sub-groups and members:
+```
+class Group():
+    def __init__(self, _name):
+        self.name = _name
+        self.groups = []
+        self.members = []
+```
+
+ The **is_member_in_group(*member, group*)** method returns **TRUE** if a member is member of the particular group or member of at least one of the sub-groups of that group. The implementation of this method is pretty straight-forward and is implemented in a recursive manner. It returns TRUE as soon as one positive membership is found and doesn't check the remaining groups.
+
+ The challenge of this exercise is not so much the implementation of the method but rather the testing methods. For testing purposes an input dictionary with groups is provided and used to construct the directory tree. In the following the dictionary is also used for the validation of the **is_member_in_group(*member, group*)** method.
+
+---
+
+### Runtime efficiency
+
+As described above, the **is_member_in_group(*member, group*)** method stops as soon as the first positive membership is found. This prevents the method from going too deep into the tree if not necessary. The runtime efficiency of course depends highly on where the member is located in the tree, which leads to best case O(1) and worst case O(n). A better estimation of the runtime efficiency would be possible if the directory tree would have been implemented as a binary search tree (which hasn't been done in this exercise). In the latter case, the worst case runtime could be estimated to be of order O(log(n))
+
+|  | Time complexity | Space complexity | Decription |
+| ------------------- | --------------- | ---------------- | ---------------- |
+| Worst case | O(n) | O(d) | If all nodes of the tree need to be searched |
+| Best case | O(1) | O(1) | If the member is member of the first group searched |
+| Worst case BST | O(log(n)) | O(log(n)) | If the directory would have been impemented as BST |
+
+**n**: total number of nodes in directory tree
+**d**: tree depths
+
+---
+
+### Test Cases
+
+The following test cases have been created to test base and corner use cases:
+
+1. Test 'Family' directory tree of depths 3 and widths 1
+2. Test 'Numbers' directorry tree of depths 4 and widths 2 plus multiple references to same group/node
+
+Running the tests can be executed using the following command:
+
+```
+python -m unittest ActiveDirectory.py
+```
